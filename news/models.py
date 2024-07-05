718bc0a202c8252f69b0ed88afd14eb48369b22d
Beekash99 <bikashkarn19@gmail.com>
2024-07-05 21:43:51 +0545

    created news model

--- news/models.py
+++ news/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class News(miodel.models):
+  title=models.CharField(max_length=255)
+  image=models.models.ImageField(upload_to="news")
+  description=models.TextField()
+  Created_date=models.DateTimeField(auto_now_add=True)
+  updated_date=models.DateTimeField(auto_now=True)
+
+  def __str__(self):
+      return self.title
+  
+  class meta():
+     varbose_name='news'
+     varbose_name_plural="news"
+  
