31e4bd8167209c86855a0369d975136d4d0dd2af
Beekash99 <bikashkarn19@gmail.com>
2024-07-08 21:40:08 +0545

    news model register and media url settings

--- news/models.py
+++ news/models.py
@@ -1,16 +1,16 @@
 from django.db import models
 
-class News(model.models):
-  title=models.CharField(max_length=255)
-  image=models.models.ImageField(upload_to="news")
-  description=models.TextField()
-  Created_date=models.DateTimeField(auto_now_add=True)
-  updated_date=models.DateTimeField(auto_now=True)
+class News(models.Model):
+  title = models.CharField(max_length=255)
+  image = models.ImageField(upload_to="news")
+  description = models.TextField()
+  Created_date = models.DateTimeField(auto_now_add=True)
+  updated_date = models.DateTimeField(auto_now=True)
 
   def __str__(self):
       return self.title
   
   class meta():
-     varbose_name='news'
-     varbose_name_plural="news"
+    verbose_name = 'news'
+    verbose_name_plural  ="news"
   
