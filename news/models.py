d0fe661667eee51ad8bfb42127ee78f90ee86b90
Beekash99 <bikashkarn19@gmail.com>
2024-07-15 21:43:55 +0545

    Tempelate Inheritance

--- news/models.py
+++ news/models.py
@@ -1,16 +1,26 @@
 from django.db import models
+class Category(models.Model):
+  name = models.CharField(max_length=255)
+  created_date = models.DateTimeField(auto_now_add=True)
+  updated_date = models.DateTimeField(auto_now=True)
 
+  def __str__(self):
+    return self.name
+  
+  class Meta:
+    verbose_name = 'Category'
+    verbose_name_plural  ="Categories"
 class News(models.Model):
   title = models.CharField(max_length=255)
   image = models.ImageField(upload_to="news")
   description = models.TextField()
-  Created_date = models.DateTimeField(auto_now_add=True)
+  created_date = models.DateTimeField(auto_now_add=True)
   updated_date = models.DateTimeField(auto_now=True)
 
   def __str__(self):
       return self.title
   
-  class meta():
+  class Meta:
     verbose_name = 'news'
     verbose_name_plural  ="news"
   
