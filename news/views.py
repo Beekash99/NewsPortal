3a3a0bd93584c0216a6e611db20c4e935d5efa2a
Beekash99 <bikashkarn19@gmail.com>
2024-07-20 22:26:28 +0545

    feat:Frontend

--- news/views.py
+++ news/views.py
@@ -2,12 +2,17 @@ from django.shortcuts import render
 from news.models import News
 
 def home_page(request):
-    editorial_news = News.objects.filter(is_editorial=True).order_by("-id")[:4]
+    editorial_news = News.objects.filter(is_editorial=True).order_by("-id")[:5]
+    trending_news = News.objects.all().order_by("-views_count")[:4]
+    popular_news = News.objects.all().order_by("-views_count")[:4]
+    recent_news = News.objects.all().order_by("-created_date")[:3]
     context = {
-        "news":"", 
-        "editorial":editorial_news,
+    "editorial":editorial_news,
+    "trending_news": trending_news,
+    "popular_news":popular_news,
+    "recent_news":recent_news,
     }
     return render(request, "index.html", context)
 
 def contact_us(request):
-  return render(request, "contactus.html")
+    return render(request, "contactus.html")
