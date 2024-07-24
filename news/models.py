51550a5dd00ad2df5882aa59ca7a669ccc1bbea2
Beekash99 <bikashkarn19@gmail.com>
2024-07-24 14:39:20 +0545

    Feat:Details Page

--- news/models.py
+++ news/models.py
@@ -1,16 +1,18 @@
 from django.db import models
 from django.contrib.auth.models import User
 class Category(models.Model):
-  name = models.CharField(max_length=255)
-  created_date = models.DateTimeField(auto_now_add=True)
-  updated_date = models.DateTimeField(auto_now=True)
+    name = models.CharField(max_length=255)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
 
-  def __str__(self):
-    return self.name
-  
-  class Meta:
-    verbose_name = 'Category'
-    verbose_name_plural  ="Categories"
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural  ="Categories"
+
+
 class News(models.Model):
     title = models.CharField(max_length=255)
     category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True, related_name="news_category")
@@ -25,24 +27,23 @@ class News(models.Model):
 
     def __str__(self):
         return self.title
-    
+
     class Meta:
         verbose_name = "news"
         verbose_name_plural = "news"
-  
-  
-class ContactUs(models.Model):
-  first_name = models.CharField(max_length=30)
-  last_name = models.CharField(max_length=30)
-  email = models.EmailField()
-  phone = models.PositiveBigIntegerField()
-  feedback_message = models.TextField()
 
-  def __str__(self):
-      return self.first_name + " " + self.last_name
-  class Meta:
-      verbose_name = "Contact Us"
-      verbose_name_plural = "Contact Us"
-  
+
+class ContactUs(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    email = models.EmailField()
+    phone = models.PositiveBigIntegerField()
+    feedback_message = models.TextField()
+
+def __str__(self):
+    return self.first_name + " " + self.last_name
+class Meta:
+    verbose_name = "Contact Us"
+    verbose_name_plural = "Contact Us"
 
 
--- news/views.py
+++ news/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from news.models import News
+from news.models import News, Category
+
 
 def home_page(request):
     editorial_news = News.objects.filter(is_editorial=True).order_by("-id")[:5]
@@ -7,14 +8,39 @@ def home_page(request):
     popular_news = News.objects.all().order_by("-views_count")[:4]
     recent_news = News.objects.all().order_by("-created_date")[:3]
     editor_picks= News.objects.filter(is_editorial=True).order_by("-id")[:4]
+    categories = Category.objects.all()
+    category_news = {
+        category.name: News.objects.filter(category=category) for category in categories
+        }
     context = {
     "editorial": editorial_news,
     "trending_news": trending_news,
     "popular_news": popular_news,
     "recent_news": recent_news,
     "editor_picks": editor_picks,
+    "category_news":category_news,
     }
     return render(request, "index.html", context)
 
+def category(request, category_id):
+    category_related_news = News.objects.filter(category=category_id)
+    context = {
+        "category_related_news":category_related_news,
+    }
+    return render(request, "category.html", context)
+
+
 def contact_us(request):
-    return render(request, "contactus.html")
+    context = {
+        "from": "",
+    }
+    return render(request, "contactus.html", context)
+
+
+
+def detail_news(request, news_id):
+    detail_news = News.objects.get(id=news_id)
+    context = {
+        "detail_news":detail_news,
+    }
+    return render(request, "details.html", context)
